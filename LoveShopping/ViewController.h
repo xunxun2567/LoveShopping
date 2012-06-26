e3b8a6aa1f3723c260bba7f66b9058995b5b571e
konglingkai <konglingkai@egibbon.com>
2012-06-26 15:20:00 +0800

    Initial Commit

---
+++ LoveShopping/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  LoveShopping
+//
+//  Created by Lingkai Kong on 12-6-26.
+//  Copyright (c) 2012年 Egibbon Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
